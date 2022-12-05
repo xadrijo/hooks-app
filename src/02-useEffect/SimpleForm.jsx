227006f4195e6470815d8f49202d30deabfa38a0
Jorge Luis <jorgeluis.jimenez@xmartlabs.com>
2022-12-05 09:50:19 -0300

    check useEffect lifecycle

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react"
+import { Message } from "./Message"
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: 'theWitcher',
+        email: 'thewitcher@gmail.com'
+    })
+
+    const { username, email } = formState
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target
+        setFormState({
+            ...formState,
+            [ name ]: value
+        })
+    }
+
+    useEffect( () => {
+        // console.log('useEffect called')
+    }, [])
+    // [] means useEffect is called the first time the component is rendered!!
+
+    useEffect( () => {
+        // console.log('formState changed')
+    }, [formState])
+
+    useEffect( () => {
+        // console.log('email changed')
+    }, [email])
+    
+
+    return (
+        <>
+            <h1>Simple Form</h1>
+            <hr />
+
+            <input 
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={ username }
+                onChange = { onInputChange }
+            />
+
+            <input 
+                type="email"
+                className="form-control mt-2"
+                placeholder="email@google.com"
+                name="email"
+                value={ email }
+                onChange = { onInputChange }
+            />  
+
+            {
+                ( username === 'strider2' ) && <Message />
+            }
+        </>
+    )
+}
